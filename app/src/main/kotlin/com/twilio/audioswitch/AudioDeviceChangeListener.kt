5be85c4635a194a06d2eca1a37a2e1b7a1f71237
L2NE-dev <conway24b@gmail.com>
2026-04-12 15:26:23 +0700

    Update modules

--- app/src/main/kotlin/com/twilio/audioswitch/AudioDeviceChangeListener.kt
+++
@@ -1,5 +0,0 @@
-package com.twilio.audioswitch
-
-interface AudioDeviceChangeListener {
-    fun onAudioDeviceChange(audioDevices: List<AudioDevice>, selectedAudioDevice: AudioDevice)
-}
--- test_uri.kt
+++
@@ -1,6 +0,0 @@
-import java.net.URI
-
-fun main() {
-    println(URI("http://:192.168.0.110:8080/clipboard").toString())
-    println(URI("http://id:192.168.0.110:8080/clipboard").toString())
-}
